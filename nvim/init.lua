128df2e93d15c4e7425972e131e4fc3465187458
prapoju <prapoju@gmail.com>
2026-03-01 13:32:56 -0500

    Vimpostor

--- nvim/init.lua
+++ nvim/init.lua
@@ -170,6 +170,21 @@ vim.keymap.set('i', '<A-10>','<C-\\><C-o>10gt', { noremap = true })
 
 vim.cmd.colorscheme('elflord')
 
+local openCmdOutInBuffer=function(opts)
+  --local filePath="/tmp/output.txt"
+  --vim.cmd("sil !touch ".. filePath)
+  --vim.cmd("redi! > " .. filePath)
+  --vim.cmd("sil !touch " .. opts.fargs[1] )
+  --vim.cmd("redi END")
+  --vim.cmd("tabedit " .. filePath)
+  --
+  vim.cmd("tabnew | put =execute('" .. opts.fargs[1] .. "')")
+end
+
+vim.api.nvim_create_user_command("Cob",openCmdOutInBuffer,{desc="Open a command output in a new buffer",nargs=1});
+
+
+-- PLUGINS
 require("lazy").setup({
   spec = {
     {
@@ -288,6 +303,10 @@ require("lazy").setup({
         'nvim-treesitter/nvim-treesitter',
         lazy = false,
         build = ':TSUpdate'
+      },
+      {
+        'vimpostor/vim-tpipeline'
+
       }
 
 
@@ -299,6 +318,24 @@ require("lazy").setup({
     checker = { enabled = false},
   })
 
+-- snippets config
+vim.cmd[[
+" press <Tab> to expand or jump in a snippet. These can also be mapped separately
+" via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+" -1 for jumping backwards.
+inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
+
+snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
+snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+" For changing choices in choiceNodes (not strictly necessary for a basic setup).
+imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+
+]]
+
+-- Load snippets from ~/.config/nvim/LuaSnip/
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
 
 
 
